5d809910933f27147845d9a6c3c0093af0c7096c
nahuely <nahuelamendola@gmail.com>
2019-09-18 16:03:26 +0545

    normalization on names in components and views

--- client/src/views/index.js
+++ client/src/views/index.js
@@ -1,19 +1,7 @@
-import AboutUs from "./about-us/AboutUs";
-import Campaign from "./campaign";
-import Login from "./login/Login";
-import Profile from "./profile/Profile";
-import Shortener from "./shortener/Shortener";
-import SignUp from "./sign-up/SignUp";
-import Stats from "./stats/Stats";
-import Links from "./Links";
+import AboutUs from "./about-us";
+import Shortener from "./shortener";
 
 export default {
   AboutUs,
-  Campaign,
-  Login,
-  Profile,
-  Shortener,
-  SignUp,
-  Stats,
-  Links
+  Shortener
 };
---
+++ client/src/views/shortener/index.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+function Shortener() {
+  return (
+    <div>
+      <button>create shortener</button>
+    </div>
+  );
+}
+
+export default Shortener;
