1be6d2e900578afecdb3690c0f5ab4e75e7dce0f
nahuely <nahuelamendola@gmail.com>
2019-09-18 17:15:33 +0545

    create shortener view added

---
+++ client/src/views/shortener/create/index.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+function CreateShortener() {
+  return (
+    <div>
+      <button>create shortener</button>
+    </div>
+  );
+}
+
+export default CreateShortener;
