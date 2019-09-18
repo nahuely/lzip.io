008e52b88dbef9be2469e673b42878edaf8dc280
nahuely <nahuelamendola@gmail.com>
2019-09-18 17:15:42 +0545

    track shortener added

---
+++ client/src/views/shortener/track/index.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+function TrackShortener() {
+  return (
+    <div>
+      <button>track shortener</button>
+    </div>
+  );
+}
+
+export default TrackShortener;
