663537b4357acc8c84944436b3fb9bd99bab5d7e
nahuely <nahuelamendola@gmail.com>
2019-09-19 14:56:15 +0545

    create shortener wip

--- client/src/views/shortener/create/index.jsx
+++ client/src/views/shortener/create/index.jsx
@@ -1,15 +1,17 @@
 import React, { useState } from "react";
+import { Button, Input } from "../../../components";
 
-function CreateShortener(props) {
+function CreateShortener({ history }) {
   const [inputs, setInputs] = useState([""]);
   const [hash, setHash] = useState("");
   const [description, setDescription] = useState("");
   const [loader, setLoader] = useState(false);
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
 
   function handleChangeInput(value, index) {
-    const text = value.target.value;
     let newState = [...inputs];
-    newState[index] = text;
+    newState[index] = value;
     setInputs(newState);
   }
 
@@ -17,11 +19,11 @@ function CreateShortener(props) {
     setInputs([...inputs, ""]);
   }
 
-  function createShortener() {
+  function handleCreateShortener() {
     const shortener = {
       links: inputs,
-      hash: hash,
-      description: description
+      hash: hash || undefined,
+      description: description || undefined
     };
     setLoader(true);
     fetch("http://127.0.0.1:8080/api/shortener", {
@@ -32,52 +34,73 @@ function CreateShortener(props) {
       body: JSON.stringify(shortener)
     })
       .then(response => response.json())
-      .then(json => {
-        console.log(json);
+      .then(({ data }) => {
+        handleResetForm();
+        setData(data.hash);
+      })
+      .catch(({ message }) => {
+        setError(message);
+      })
+      .finally(() => {
         setLoader(false);
       });
+  }
+
+  function handleResetForm() {
+    setInputs([""]);
+    setHash("");
+    setDescription("");
   }
 
   if (loader)
     return (
       <div>
-        <p>loading</p>
+        <p>creating shortener</p>
       </div>
     );
 
+  if (data) {
+    return (
+      <div>
+        <p>{data}</p>
+        <Button onClick={() => setData(null)}>create new shortener</Button>
+      </div>
+    );
+  }
+
   return (
     <div>
-      <input
+      <Input
         value={hash}
         onChange={value => setHash(value.target.value)}
         placeholder="customize hash"
       />
-      <input
+      <br />
+      <Input
         value={description}
         onChange={value => setDescription(value.target.value)}
         placeholder="add description"
       />
+      <br />
       {inputs.map((value, index) => {
         return (
-          <input
-            key={index}
-            value={value}
-            onChange={value => handleChangeInput(value, index)}
-            placeholder="add url"
-          />
+          <div key={index}>
+            <Input
+              value={value}
+              onChange={value => handleChangeInput(value.target.value, index)}
+              placeholder="add url"
+            />
+          </div>
         );
       })}
 
       <br />
-      <button onClick={() => handleAddInput()}>add more inputs</button>
+      <Button onClick={handleAddInput}>add more inputs</Button>
       <br />
-      <button
-        onClick={() => {
-          createShortener();
-        }}
-      >
-        create shortener
-      </button>
+      <Button onClick={handleCreateShortener}>create shortener</Button>
+      <Button onClick={handleResetForm}>reset</Button>
+      <br />
+      <Button onClick={() => history.push("/")}>back home</Button>
     </div>
   );
 }
