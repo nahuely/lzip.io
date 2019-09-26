96a10ceb7df3b35fcbc6f0304028a9f5cb2b1a50
nahuely <nahuelamendola@gmail.com>
2019-09-26 19:24:12 +0545

    usefetch improved, abort request on cleanup function

--- client/src/hooks/index.js
+++ client/src/hooks/index.js
@@ -6,17 +6,33 @@ export const useFetch = url => {
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    setLoader(true);
-    fetch(url)
-      .then(response => response.json())
-      .then(({ code, status, data }) => {
-        if (code === 404) throw new Error(status);
+    const abortCtrl = new AbortController();
+    let abortError = false;
+
+    async function doFetch() {
+      try {
+        setLoader(true);
+        const response = await fetch(url, { signal: abortCtrl.signal });
+        const { code, data, message } = await response.json();
+        if (code === 404) throw new Error(message);
         setData(data);
-      })
-      .catch(setError)
-      .finally(() => {
-        setLoader(false);
-      });
+      } catch (error) {
+        if (error.name !== "AbortError") {
+          setError(error);
+        } else {
+          abortError = true;
+        }
+      } finally {
+        if (!abortError) {
+          setLoader(false);
+        }
+      }
+    }
+    doFetch();
+
+    return () => {
+      abortCtrl.abort();
+    };
   }, [url]);
 
   return [loader, error, data];
