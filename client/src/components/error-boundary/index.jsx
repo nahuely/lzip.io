5fe542481010558954310e49b4825a765d0a0d2b
nahuely <nahuelamendola@gmail.com>
2019-09-29 01:38:32 +0545

    remove console logs and comments

--- client/src/components/error-boundary/index.jsx
+++ client/src/components/error-boundary/index.jsx
@@ -7,18 +7,13 @@ class ErrorBoundary extends Component {
   }
 
   static getDerivedStateFromError(error) {
-    // Update state so the next render will show the fallback UI.
     return { hasError: true };
   }
 
-  componentDidCatch(error, info) {
-    // You can also log the error to an error reporting service
-    console.log(error, info);
-  }
+  componentDidCatch(error, info) {}
 
   render() {
     if (this.state.hasError) {
-      // You can render any custom fallback UI
       return <h1>Something went wrong.</h1>;
     }
 
