142a1d70bef54d0909898c5ae16036587aacc3d5
nahuely <nahuelamendola@gmail.com>
2019-09-29 22:31:35 +0545

    loader new styles

--- client/src/components/loader/index.jsx
+++ client/src/components/loader/index.jsx
@@ -4,7 +4,7 @@ import "./styles.scss";
 function Loader() {
   return (
     <div className="loader loader__layover">
-      <p className="loader__spinner">loadding ...</p>
+      <p className="loader__spinner">processing ...</p>
     </div>
   );
 }
