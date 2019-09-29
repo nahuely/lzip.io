120a208ba07a04b384b03f938a035d5c5ea25f60
nahuely <nahuelamendola@gmail.com>
2019-09-29 22:23:29 +0545

    not found view

--- client/src/views/not-found/index.jsx
+++ client/src/views/not-found/index.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { Button } from "../../components";
+import "./styles.scss";
 
 function NotFound() {
+  function goHome() {
+    window.location.replace("/");
+  }
+
   return (
-    <div>
-      <p>not found</p>
+    <div className="view view__container not-found">
+      <p className="not-found__message">Not found &#129300;</p>
+      <p className="not-found__explanation">
+        sorry, we couldn't find the url that you typed
+      </p>
+      <Button onClick={goHome}>go home</Button>
     </div>
   );
 }
