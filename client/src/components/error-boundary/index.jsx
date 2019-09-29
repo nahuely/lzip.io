9bbb4420b3680ab0a68ad568204157399ed42145
nahuely <nahuelamendola@gmail.com>
2019-09-29 22:23:13 +0545

    error boundary styles

--- client/src/components/error-boundary/index.jsx
+++ client/src/components/error-boundary/index.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { Button } from "../../components";
+import "./styles.scss";
 
 class ErrorBoundary extends Component {
   constructor(props) {
@@ -12,9 +14,24 @@ class ErrorBoundary extends Component {
 
   componentDidCatch(error, info) {}
 
+  reloadApp() {
+    window.location.replace("/");
+  }
+
   render() {
     if (this.state.hasError) {
-      return <h1>Something went wrong.</h1>;
+      return (
+        <div className="view view__container error-boundary">
+          <p className="error-boundary__message">
+            Sorry, something went wrong &#128577;
+          </p>
+          <p className="error-boundary__explanation">
+            click reload app button, if the problem persist send an email to{" "}
+            <a href="mailto:support@lzip.io">support@lzip.io</a>
+          </p>
+          <Button onClick={this.reloadApp}>reload app</Button>
+        </div>
+      );
     }
 
     return this.props.children;
